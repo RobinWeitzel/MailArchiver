8573b5c112c81decc6772b6e3797a6bfb6236d35
Robin Weitzel <robin.weitzel@fim-rc.de>
2019-02-22 20:11:42 +0200

    Fixed bug where script would crash if the mail did not contain any text

--- server.js
+++ server.js
@@ -8,26 +8,29 @@ const imap = {
   host: process.env.host, 
   port: process.env.port, // 993
   tls: true,// use secure connection
-  tlsOptions: { rejectUnauthorized: false }
+  tlsOptions: { rejectUnauthorized: false },
+  markSeen: false
 };
 
 n = notifier(imap);
 
 n.on('end', () => n.start()) // session closed
     .on('mail', mail => {
-        const root = "../archive"; // To bind, use -v /storage/mail_archiver:/archive
-        const inbox = process.env.name;
-        const year = mail.date.getFullYear();
-        const month = mail.date.getMonth() + 1;
-        let day = mail.date.getDay();
-        if(day === 0) {
-            day = 7;
-        }
-        const subject = sanitize(mail.subject);
-        const path = root + "/" + inbox + "/" + year + "/" + month + "/" + day + "/" + subject + "/";
+        if(mail !== undefined) {
+            const root = "../archive"; // To bind, use -v /storage/mail_archiver:/archive
+            const inbox = process.env.name;
+            const year = mail.date.getFullYear();
+            const month = mail.date.getMonth() + 1;
+            let day = mail.date.getDay();
+            if(day === 0) {
+                day = 7;
+            }
+            const subject = sanitize(mail.subject);
+            const path = root + "/" + inbox + "/" + year + "/" + month + "/" + day + "/" + subject + "/";
 
-        jetpack.write(path + "email.json", JSON.stringify(mail));
-        jetpack.write(path + "email.txt", mail.text);
-        jetpack.write(path + "email.html", mail.html);
+            jetpack.write(path + "email.json", JSON.stringify(mail));
+            jetpack.write(path + "email.txt", mail.text || "");
+            jetpack.write(path + "email.html", mail.html || "");
+        }
     })
     .start();
