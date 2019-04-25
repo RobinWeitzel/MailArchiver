faf71459224aaeafaf568ee3ca05b46a304551c1
Robin Weitzel <robin.weitzel@fim-rc.de>
2019-04-25 14:22:16 +0300

    fixed bug where connection to mongo would time out

--- server.js
+++ server.js
@@ -5,64 +5,109 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const imap = {
-  user: process.env.user, 
-  password: process.env.password, 
-  host: process.env.host, 
-  port: process.env.port, // 993
-  tls: true,// use secure connection
-  tlsOptions: { rejectUnauthorized: false },
-  markSeen: false
+    user: process.env.user,
+    password: process.env.password,
+    host: process.env.host,
+    port: process.env.port, // 993
+    tls: true,// use secure connection
+    tlsOptions: { rejectUnauthorized: false },
+    markSeen: false
 };
 
 const url = process.env.mongourl; // i.e. 'mongodb://localhost:27017'
 const dbName = process.env.mongodb;
 const inboxName = process.env.name;
 
-// Create a new MongoClient
-let client;
-if(url !== 'url') {
-    client = new MongoClient(url, { useNewUrlParser: true });
-}
-
 n = notifier(imap);
 
-n.on('end', () => n.start()) // session closed
-    .on('mail', mail => {
-        if(mail !== undefined) {
-            const root = "../archive"; // To bind, use -v /storage/mail_archiver:/archive
-            const inbox = inboxName;
-            const year = mail.date.getFullYear();
-            const month = mail.date.getMonth() + 1;
-            let day = mail.date.getDate();
-            if(day === 0) {
-                day = 7;
-            }
-            let subject = sanitize(mail.subject || "empty-subject");
-            if(subject === "") {
-                subject = "empty-subject";
-            }
-            const id = mail.uid;
+// Create a new MongoClient
+if (url !== 'url') {
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
+        if (err) throw err;
 
-            const path = root + "/" + inbox + "/" + year + "/" + month + "/" + day + "/" + subject + "/" + id + "/";
+        const client = database;
+        const db = client.db(dbName);
 
-            jetpack.write(path + "email.json", JSON.stringify(mail));
-            jetpack.write(path + "email.txt", mail.text || "");
-            jetpack.write(path + "email.html", mail.html || "");
+        n.on('end', () => n.start()) // session closed
+            .on('mail', mail => {
+                if (mail !== undefined) {
+                    const root = "../archive"; // To bind, use -v /storage/mail_archiver:/archive
+                    const inbox = inboxName;
+                    const year = mail.date.getFullYear();
+                    const month = mail.date.getMonth() + 1;
+                    let day = mail.date.getDate();
+                    if (day === 0) {
+                        day = 7;
+                    }
+                    let subject = sanitize(mail.subject || "empty-subject");
+                    if (subject === "") {
+                        subject = "empty-subject";
+                    }
+                    const id = mail.uid;
 
-            if(client !== undefined) {
-                client.connect(function(err) {
-                    assert.equal(null, err);
-                  
-                    const db = client.db(dbName);
-                  
-                    const collection = db.collection(inbox);
+                    const path = root + "/" + inbox + "/" + year + "/" + month + "/" + day + "/" + subject + "/" + id + "/";
 
-                    collection.insertOne(mail, (err, result) => {
-                        client.close();
+                    jetpack.write(path + "email.json", JSON.stringify(mail));
+                    jetpack.write(path + "email.txt", mail.text || "");
+                    jetpack.write(path + "email.html", mail.html || "");
+
+                    if (client !== undefined) {
+                        client.connect(function (err) {
+                            assert.equal(null, err);
+                            
+                            const collection = db.collection(inbox);
+
+                            collection.insertOne(mail);
+                        });
+                    }
+
+                }
+            })
+            .start();
+    });
+} else {
+    n.on('end', () => n.start()) // session closed
+        .on('mail', mail => {
+            if (mail !== undefined) {
+                const root = "../archive"; // To bind, use -v /storage/mail_archiver:/archive
+                const inbox = inboxName;
+                const year = mail.date.getFullYear();
+                const month = mail.date.getMonth() + 1;
+                let day = mail.date.getDate();
+                if (day === 0) {
+                    day = 7;
+                }
+                let subject = sanitize(mail.subject || "empty-subject");
+                if (subject === "") {
+                    subject = "empty-subject";
+                }
+                const id = mail.uid;
+
+                const path = root + "/" + inbox + "/" + year + "/" + month + "/" + day + "/" + subject + "/" + id + "/";
+
+                jetpack.write(path + "email.json", JSON.stringify(mail));
+                jetpack.write(path + "email.txt", mail.text || "");
+                jetpack.write(path + "email.html", mail.html || "");
+
+                if (client !== undefined) {
+                    client.connect(function (err) {
+                        assert.equal(null, err);
+
+                        const db = client.db(dbName);
+
+                        const collection = db.collection(inbox);
+
+                        collection.insertOne(mail, (err, result) => {
+                            client.close();
+                        });
                     });
-                });
-            }
+                }
 
-        }
-    })
-    .start();
+            }
+        })
+        .start();
+}
+
+
+
+
